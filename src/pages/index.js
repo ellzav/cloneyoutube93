cc713ec7eb208c6d376e78a9ed2a0ef333cf8838
ellzav <daniel_vaz_@hotmail.com>
2021-04-17 08:03:43 +0100

    Material-UI CONFIGURATION

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
+     
       clone Youtube next js
+      <Button variant="outline" color="primary">
+  Primary
+</Button>
     </div>
   );
 }
